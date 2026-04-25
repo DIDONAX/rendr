2e2d36aa2f444409da35ade71a3d612d3f7b576b
DIDONAX <didounimedamine@gmail.com>
2026-04-25 17:29:51 +0200

    fix constants

--- include/rendr/constants.h
+++ include/rendr/constants.h
@@ -11,8 +11,8 @@ constexpr auto kIndexCapacity = 50;
 constexpr auto kVertexCapacity = 15;
 constexpr auto kMeshCapacity= 3;
 
-constexpr auto kInstanceCapacity = 1000000;
-static_assert(kIndexCapacity < std::numeric_limits<uint>::max());
+constexpr auto kInstanceCapacity = 1000000U;
+static_assert(kInstanceCapacity < std::numeric_limits<uint>::max());
 
 constexpr color_t White = {1,1,1,1};
 constexpr color_t Red = {1,0,0,1};
